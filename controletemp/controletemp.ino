4a064ca3e3ac5fe2d44261391915f42e09bbcab1
Henrique <cerevisiae.poli@gmail.com>
2022-11-18 16:22:29 -0300

    adicionei ph

--- controletemp/controletemp.ino
+++ controletemp/controletemp.ino
@@ -22,7 +22,12 @@ int tempo = 0;             // Tempo em que a medição é realizada
 const int thermoDO = 8;    // Pino digital conectado a porta SO do MAX6675
 const int thermoCS = 9;    // Pino digital conectado a porta CS do MAX6675
 const int thermoCLK = 10;  // Pino digital conectado a porta SCK do MAX6675
-
+int contagem = 0;           // Variável de contagem
+float soma_tensao = 0;      // Variável para soma de tensão
+float media = 0;            // Variável que calcula a media
+float entrada_A0;           // Variável de leitura do pino A0
+float tensao;               // Variável para conversão em tensão
+unsigned long time;        // Float tempo
 #define DHTPIN pinoDHT
 #define DHTTYPE DHT11
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
@@ -66,11 +71,25 @@ void setup() {
 
 void loop() {
   // Wait a few seconds between measurements.
+  soma_tensao = 0;   // Inicia soma_tensão em 0
+  contagem = 0;      // Inicia a contagem em 0
   delay(dt);
   display.clearDisplay();
   buttonState = digitalRead(buttonPin);
   if (iniciou == 1 || buttonState == 1)       { 
     iniciou = 1;
+    while (contagem < 10) {                   // Executa enquanto contagem menor que 10
+      time = millis();                       // Define o tempo em microssegundos
+      entrada_A0 = analogRead(A0);            // Lê a entrada analógica
+      tensao = (entrada_A0 * 5.0) / 1024.0;   // Converte em tensão, o valor lido
+      soma_tensao = (soma_tensao + tensao);   // Soma a tensão anterior com a atual
+      contagem++;                             // Soma 1 à variável de contagem
+      delay(100);                             // Aguarda para próxima leitura
+    }
+ 
+    media = soma_tensao / 10;                 // Calcula a média das leituras
+ 
+    float valor_pH =  media ;    // Calcula valor de pH  
 
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
     float h = dht.readHumidity();
@@ -104,14 +123,16 @@ void loop() {
     display.setTextSize(2);
     display.setTextColor(WHITE);    
     display.setCursor(0, 0);
-    display.println("Cerevisiae");
+  //display.print("Cerevisiae") ;   
     display.print("Ti:");
     display.println(ti);
     display.print("Te:");
     display.println(te);
     display.print("U:");
-    display.println(u);
-    display.display();
+    display.println(u); 
+    display.print("pH:");
+    display.println(valor_pH);
+    display.display();    
   }
-  display.display();  
+    display.display();
 }
